60873fc37227f783075a4f7992c3d256b045459b
Kenny Flegal <soulstompp@gmail.com>
2019-05-20 04:03:46 +0000

    break up query fn

--- sql-composer-cli/src/main.rs
+++ sql-composer-cli/src/main.rs
@@ -1,7 +1,7 @@
 use quicli::prelude::*;
 use structopt::StructOpt;
 
-use sql_composer::composer::{ComposerBuilder, ComposerConnection};
+use sql_composer::composer::ComposerConnection;
 use sql_composer::types::{value::Value, SqlComposition};
 use std::collections::{BTreeMap, HashMap};
 
@@ -110,140 +110,11 @@ fn query(args: QueryArgs) -> CliResult {
     if uri.starts_with("mysql://") {
         query_mysql(uri, comp, parsed_values)?;
     }
-    else if uri.starts_with("sqlite://") {
-        //TODO: base off of uri
-        let conn = match uri.as_str() {
-            "sqlite://:memory:" => RusqliteConnection::open_in_memory().unwrap(),
-            _ => unimplemented!("not currently passing uri correctly"),
-        };
-
-        let values: BTreeMap<String, Vec<&RusqliteToSql>> =
-            parsed_values
-                .iter()
-                .fold(BTreeMap::new(), |mut acc, (k, v)| {
-                    let entry = acc.entry(k.to_string()).or_insert(vec![]);
-                    *entry = v.iter().map(|x| x as &RusqliteToSql).collect();
-
-                    acc
-                });
-
-        let (mut prep_stmt, bindings) =
-            conn.compose(&comp, values, vec![], HashMap::new()).unwrap();
-
-        let column_names: Vec<String> = prep_stmt
-            .column_names()
-            .into_iter()
-            .map(String::from)
-            .collect();
-
-        let driver_rows = prep_stmt
-            .query_map(&bindings, |driver_row| {
-                let map = column_names.iter().enumerate().fold(
-                    BTreeMap::new(),
-                    |mut acc, (i, column_name)| {
-                        let _ = acc
-                            .entry(SerdeValue::String(column_name.to_string()))
-                            .or_insert(match driver_row.get_raw(i) {
-                                RusqliteValueRef::Null => SerdeValue::Unit,
-                                RusqliteValueRef::Integer(int) => SerdeValue::I64(int),
-                                RusqliteValueRef::Real(r) => SerdeValue::F64(r),
-                                RusqliteValueRef::Text(t) => SerdeValue::String(t.to_string()),
-                                RusqliteValueRef::Blob(vc) => {
-                                    let s = std::string::String::from_utf8(vc.to_vec()).unwrap();
-
-                                    SerdeValue::String(s)
-                                }
-                            });
-
-                        acc
-                    },
-                );
-
-                Ok(map)
-            })
-            .unwrap();
-
-        let mut seq = vec![];
-
-        for driver_row in driver_rows {
-            seq.push(SerdeValue::Map(driver_row.unwrap()));
-        }
-
-        output(SerdeValue::Seq(seq));
-    }
     else if uri.starts_with("postgres://") {
-        let conn =
-            PgConnection::connect("postgres://vagrant:vagrant@localhost:5432", PgTlsMode::None)
-                .unwrap();
-
-        let values: BTreeMap<String, Vec<&PgToSql>> =
-            parsed_values
-                .iter()
-                .fold(BTreeMap::new(), |mut acc, (k, v)| {
-                    let entry = acc.entry(k.to_string()).or_insert(vec![]);
-                    *entry = v.iter().map(|x| x as &PgToSql).collect();
-
-                    acc
-                });
-
-        let (mut prep_stmt, bindings) =
-            conn.compose(&comp, values, vec![], HashMap::new()).unwrap();
-
-        let mut values: Vec<Vec<String>> = vec![];
-
-        let driver_rows = &prep_stmt.query(&bindings).unwrap();
-
-        let vv = driver_rows
-            .iter()
-            .fold(vec![], |mut value_maps, driver_row| {
-                let bt: BTreeMap<SerdeValue, SerdeValue> = driver_row
-                    .columns()
-                    .iter()
-                    .enumerate()
-                    .fold(BTreeMap::new(), |mut acc, (i, column)| {
-                        let v = match *column.type_() {
-                            pg_types::BOOL => {
-                                SerdeValue::Bool(driver_row.get_opt(i).unwrap().unwrap())
-                            }
-                            pg_types::CHAR => {
-                                SerdeValue::I8(driver_row.get_opt(i).unwrap().unwrap())
-                            }
-                            pg_types::INT2 => {
-                                SerdeValue::I16(driver_row.get_opt(i).unwrap().unwrap())
-                            }
-                            pg_types::INT4 => {
-                                SerdeValue::I32(driver_row.get_opt(i).unwrap().unwrap())
-                            }
-                            pg_types::OID => {
-                                SerdeValue::U32(driver_row.get_opt(i).unwrap().unwrap())
-                            }
-                            pg_types::INT8 => {
-                                SerdeValue::I64(driver_row.get_opt(i).unwrap().unwrap())
-                            }
-                            pg_types::VARCHAR | pg_types::TEXT | pg_types::NAME => {
-                                SerdeValue::String(driver_row.get_opt(i).unwrap().unwrap())
-                            }
-                            pg_types::FLOAT4 => {
-                                SerdeValue::F32(driver_row.get_opt(i).unwrap().unwrap())
-                            }
-                            pg_types::FLOAT8 => {
-                                SerdeValue::F64(driver_row.get_opt(i).unwrap().unwrap())
-                            }
-                            _ => unreachable!("shouldn't get here!"),
-                        };
-
-                        let _ = acc
-                            .entry(SerdeValue::String(column.name().to_string()))
-                            .or_insert(v);
-
-                        acc
-                    });
-
-                value_maps.push(SerdeValue::Map(bt));
-                value_maps
-            });
-
-        output(SerdeValue::Seq(vv));
+        query_postgres(uri, comp, parsed_values)?;
+    }
+    else if uri.starts_with("sqlite://") {
+        query_rusqlite(uri, comp, parsed_values)?;
     }
     else {
         panic!("unknown uri type: {}", uri);
@@ -339,6 +210,136 @@ fn query_mysql(
         });
 
     output(SerdeValue::Seq(vv));
+
+    Ok(())
+}
+
+fn query_postgres(
+    uri: String,
+    comp: SqlComposition,
+    params: BTreeMap<String, Vec<Value>>,
+) -> CliResult {
+    let conn = PgConnection::connect("postgres://vagrant:vagrant@localhost:5432", PgTlsMode::None)
+        .unwrap();
+
+    let values: BTreeMap<String, Vec<&PgToSql>> =
+        params.iter().fold(BTreeMap::new(), |mut acc, (k, v)| {
+            let entry = acc.entry(k.to_string()).or_insert(vec![]);
+            *entry = v.iter().map(|x| x as &PgToSql).collect();
+
+            acc
+        });
+
+    let (mut prep_stmt, bindings) = conn.compose(&comp, values, vec![], HashMap::new()).unwrap();
+
+    let mut values: Vec<Vec<String>> = vec![];
+
+    let driver_rows = &prep_stmt.query(&bindings).unwrap();
+
+    let vv = driver_rows
+        .iter()
+        .fold(vec![], |mut value_maps, driver_row| {
+            let bt: BTreeMap<SerdeValue, SerdeValue> = driver_row
+                .columns()
+                .iter()
+                .enumerate()
+                .fold(BTreeMap::new(), |mut acc, (i, column)| {
+                    let v = match *column.type_() {
+                        pg_types::BOOL => SerdeValue::Bool(driver_row.get_opt(i).unwrap().unwrap()),
+                        pg_types::CHAR => SerdeValue::I8(driver_row.get_opt(i).unwrap().unwrap()),
+                        pg_types::INT2 => SerdeValue::I16(driver_row.get_opt(i).unwrap().unwrap()),
+                        pg_types::INT4 => SerdeValue::I32(driver_row.get_opt(i).unwrap().unwrap()),
+                        pg_types::OID => SerdeValue::U32(driver_row.get_opt(i).unwrap().unwrap()),
+                        pg_types::INT8 => SerdeValue::I64(driver_row.get_opt(i).unwrap().unwrap()),
+                        pg_types::VARCHAR | pg_types::TEXT | pg_types::NAME => {
+                            SerdeValue::String(driver_row.get_opt(i).unwrap().unwrap())
+                        }
+                        pg_types::FLOAT4 => {
+                            SerdeValue::F32(driver_row.get_opt(i).unwrap().unwrap())
+                        }
+                        pg_types::FLOAT8 => {
+                            SerdeValue::F64(driver_row.get_opt(i).unwrap().unwrap())
+                        }
+                        _ => unreachable!("shouldn't get here!"),
+                    };
+
+                    let _ = acc
+                        .entry(SerdeValue::String(column.name().to_string()))
+                        .or_insert(v);
+
+                    acc
+                });
+
+            value_maps.push(SerdeValue::Map(bt));
+            value_maps
+        });
+
+    output(SerdeValue::Seq(vv));
+
+    Ok(())
+}
+
+fn query_rusqlite(
+    uri: String,
+    comp: SqlComposition,
+    params: BTreeMap<String, Vec<Value>>,
+) -> CliResult {
+    //TODO: base off of uri
+    let conn = match uri.as_str() {
+        "sqlite://:memory:" => RusqliteConnection::open_in_memory().unwrap(),
+        _ => unimplemented!("not currently passing uri correctly"),
+    };
+
+    let values: BTreeMap<String, Vec<&RusqliteToSql>> =
+        params.iter().fold(BTreeMap::new(), |mut acc, (k, v)| {
+            let entry = acc.entry(k.to_string()).or_insert(vec![]);
+            *entry = v.iter().map(|x| x as &RusqliteToSql).collect();
+
+            acc
+        });
+
+    let (mut prep_stmt, bindings) = conn.compose(&comp, values, vec![], HashMap::new()).unwrap();
+
+    let column_names: Vec<String> = prep_stmt
+        .column_names()
+        .into_iter()
+        .map(String::from)
+        .collect();
+
+    let driver_rows = prep_stmt
+        .query_map(&bindings, |driver_row| {
+            let map = column_names.iter().enumerate().fold(
+                BTreeMap::new(),
+                |mut acc, (i, column_name)| {
+                    let _ = acc
+                        .entry(SerdeValue::String(column_name.to_string()))
+                        .or_insert(match driver_row.get_raw(i) {
+                            RusqliteValueRef::Null => SerdeValue::Unit,
+                            RusqliteValueRef::Integer(int) => SerdeValue::I64(int),
+                            RusqliteValueRef::Real(r) => SerdeValue::F64(r),
+                            RusqliteValueRef::Text(t) => SerdeValue::String(t.to_string()),
+                            RusqliteValueRef::Blob(vc) => {
+                                let s = std::string::String::from_utf8(vc.to_vec()).unwrap();
+
+                                SerdeValue::String(s)
+                            }
+                        });
+
+                    acc
+                },
+            );
+
+            Ok(map)
+        })
+        .unwrap();
+
+    let mut seq = vec![];
+
+    for driver_row in driver_rows {
+        seq.push(SerdeValue::Map(driver_row.unwrap()));
+    }
+
+    output(SerdeValue::Seq(seq));
 
     Ok(())
 }

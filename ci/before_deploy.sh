2d0d716732db8b426017d1b50af1cb35cb6d41b0
Kenny Flegal <soulstompp@gmail.com>
2019-09-22 20:46:47 +0000

    fill out the basics of the trust config templates

---
+++ ci/before_deploy.sh
@@ -0,0 +1,33 @@
+# This script takes care of building your crate and packaging it for release
+
+set -ex
+
+main() {
+    local src=$(pwd) \
+          stage=
+
+    case $TRAVIS_OS_NAME in
+        linux)
+            stage=$(mktemp -d)
+            ;;
+        osx)
+            stage=$(mktemp -d -t tmp)
+            ;;
+    esac
+
+    test -f Cargo.lock || cargo generate-lockfile
+
+    # TODO: this should build the ci and so files
+    #cross rustc --bin hello --target $TARGET --release -- -C lto
+
+    # TODO: this should copy the ci and so files
+    cp target/$TARGET/release/hello $stage/
+
+    cd $stage
+    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
+    cd $src
+
+    rm -rf $stage
+}
+
+main

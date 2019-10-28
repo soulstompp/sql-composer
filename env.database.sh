5c3014130e8c95b63b5dd66d65f609a59971c4e9
Andrew Grangaard <granny-github@ofb.net>
2019-10-28 02:45:44 -0700

    delete env.database.sh after switch to fixed port mappings

--- env.database.sh
+++
@@ -1,4 +0,0 @@
-# source this file into your shell to set ENV variables after running docker-compose up:
-#     . ./env.database.sh
-export MYSQL_DATABASE_URL=mysql://runner@$(docker port sql_composer_mysql 3306)/sql_composer
-export PG_DATABASE_URL=postgresql://runner@$(docker port sql_composer_postgres 5432)/sql_composer
